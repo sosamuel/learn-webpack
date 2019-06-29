cf7467db44ac3ef59993a5bd6dacc407a30fcf2c
SamuelChia <funwithes6@gmail.com>
2019-06-29 10:54:32 +0800

    🎉 init project

---
+++ scripts/start.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const webpack = require("webpack");
+const open = require("open");
+
+const dirs = fs.readdirSync("./examples");
+
+if (!dirs.length) {
+  console.log("you have no example here, bye~ ");
+  process.exit();
+}
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "pack",
+      message: "What do you want to build?",
+      choices: dirs
+    }
+  ])
+  .then(answers => {
+    const dir = path.resolve(__dirname, "examples", answers.pack);
+    const config = require(path.resolve(dir, "webpack.config.js"));
+    webpack(config, async (err, stats) => {
+      if (err || stats.hasErrors()) {
+        console.error(stats.toJson().errors);
+        return;
+      }
+      console.log("build successfully!");
+      const url = path.resolve(dir, "build", "index.html");
+      console.log(`open ${url} in browser`);
+      await open(url);
+    });
+  });
